1d4d8a69810b1c871c86e802488811b52b3ff68c
Yusuf Khan <67281394+y-u-s-u-f@users.noreply.github.com>
2024-07-11 07:53:02 +0000

    Still doesn't work :(

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import lightbulb
+import logging
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = lightbulb.BotApp(token=os.getenv("DISCORD_TOKEN"), prefix="!")
+bot = lightbulb.BotApp(token=os.getenv("DISCORD_TOKEN"),
+											 prefix="!",
+											 logs={
+													 "version": 1,
+													 "incremental": True,
+													 "loggers": {
+															 "hikari": {
+																	 "level": "INFO"
+															 },
+															 "hikari.ratelimits": {
+																	 "level": "TRACE_HIKARI"
+															 },
+															 "lightbulb": {
+																	 "level": "INFO"
+															 },
+													 },
+											 })
+
 
 @bot.command
 @lightbulb.command("ping", "checks the bot is alive")
 @lightbulb.implements(lightbulb.PrefixCommand)
 async def ping(ctx: lightbulb.Context) -> None:
 		await ctx.respond("Pong!")
+
 
 bot.run()
