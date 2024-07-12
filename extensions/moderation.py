3e8d32b07468876502b68ca1c652dbc5ed0355b4
Yusuf Khan <67281394+y-u-s-u-f@users.noreply.github.com>
2024-07-12 19:23:39 +0000

    kicking finally works!

--- extensions/moderation.py
+++ extensions/moderation.py
@@ -6,14 +6,23 @@ plugin = lightbulb.Plugin("Moderation")
 
 @plugin.command
 @lightbulb.option('member', 'The member to kick', type=hikari.User)
-@lightbulb.option('reason', 'The reason for kicking the member')
+@lightbulb.option('reason',
+                  'The reason for kicking the member',
+                  required=False)
 @lightbulb.command(name='kick', description='Kick a user from the server.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def kick(ctx: lightbulb.SlashContext):
-		await ctx.respond(hikari.ResponseType.DEFERRED_MESSAGE_CREATE)
-		await ctx.options.member.send(embed=hikari.Embed(title='Kicked!', description=f'You have been kicked from {ctx.get_guild().name}', color=hikari.Color.red()))
-		await ctx.get_guild().kick_member(user=ctx.options.member, reason=reason)
-		await ctx.reply(embed=hikari.Embed(title='Kicked!', description=f'Kicked {member.mention}.', color=discord.Color.green()))
+	await ctx.respond(hikari.ResponseType.DEFERRED_MESSAGE_CREATE)
+	await ctx.options.member.send(embed=hikari.Embed(
+	    title='Kicked!',
+	    description=f'You have been kicked from {ctx.get_guild().name}',
+	    color=hikari.Color.from_rgb(255, 0, 0)))
+	await ctx.get_guild().kick(ctx.options.member,
+	                           reason=ctx.options.reason or hikari.UNDEFINED)
+	await ctx.respond(
+	    embed=hikari.Embed(title='Kicked!',
+	                       description=f'Kicked {ctx.options.member.mention}.',
+	                       color=hikari.Color.from_rgb(255, 0, 0)))
 
 
 def load(bot):
