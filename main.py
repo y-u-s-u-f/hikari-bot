7f6bd37fb226ab04972807515c8c8b341ebba5e1
Yusuf Khan <67281394+y-u-s-u-f@users.noreply.github.com>
2024-07-12 13:30:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 bot = lightbulb.BotApp(token=os.getenv("DISCORD_TOKEN"),
-											 prefix="!",
-											 logs={
-													 "version": 1,
-													 "incremental": True, 
-													 "loggers": {
-															 "hikari": {
-																	 "level": "INFO"
-															 },
-															 "hikari.ratelimits": {
-																	 "level": "TRACE_HIKARI"
-															 },
-															 "lightbulb": {
-																	 "level": "INFO"
-															 },
-													 },
-											 }, intents=hikari.Intents.ALL)
+                       prefix="!",
+                       logs={
+                           "version": 1,
+                           "incremental": True,
+                           "loggers": {
+                               "hikari": {
+                                   "level": "INFO"
+                               },
+                               "hikari.ratelimits": {
+                                   "level": "TRACE_HIKARI"
+                               },
+                               "lightbulb": {
+                                   "level": "INFO"
+                               },
+                           },
+                       },
+                       intents=hikari.Intents.ALL)
 
 
 @bot.command()
 @lightbulb.command("ping", "Checks that the bot is alive")
 @lightbulb.implements(lightbulb.PrefixCommand)
-async def ping(ctx: lightbulb.Context) -> None:
-		"""Checks that the bot is alive"""
-		await ctx.respond("Pong!")
-
+async def ping(ctx: lightbulb.Context):
+	"""Checks that the bot is alive"""
+	await ctx.respond("Pong!")
 
 
 bot.run()
