1c5b7be9ebb324b48e887c1cdcd68657932bc161
Yusuf Khan <67281394+y-u-s-u-f@users.noreply.github.com>
2024-07-12 17:36:32 +0000

    continue work on kick command

--- extensions/moderation.py
+++ extensions/moderation.py
@@ -10,9 +10,10 @@ plugin = lightbulb.Plugin("Moderation")
 @lightbulb.command(name='kick', description='Kick a user from the server.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def kick(ctx: lightbulb.SlashContext):
-		await ctx.options.member.send(embed=discord.Embed(title='Kicked!', description=f'You have been kicked from MacApps', color=discord.Color.red()))
-		await member.kick(reason=reason)
-		await interaction.followup.send(embed=discord.Embed(title='Kicked!', description=f'Kicked {member.mention}.', color=discord.Color.green()))
+		await ctx.respond(hikari.ResponseType.DEFERRED_MESSAGE_CREATE)
+		await ctx.options.member.send(embed=hikari.Embed(title='Kicked!', description=f'You have been kicked from {ctx.get_guild().name}', color=hikari.Color.red()))
+		await ctx.get_guild().kick_member(user=ctx.options.member, reason=reason)
+		await ctx.reply(embed=hikari.Embed(title='Kicked!', description=f'Kicked {member.mention}.', color=discord.Color.green()))
 
 
 def load(bot):
